dfe2e0810b5fddcde7ea36a72f33eea0ba18e6ef
jorgemonraz <jmonraz@student.fullsail.edu>
2024-01-24 15:21:19 -0800

    added flask-core package

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from ai import assign_department_and_category
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/', methods=['POST'])
